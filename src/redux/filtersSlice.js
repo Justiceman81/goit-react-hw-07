c41c4177b93113f0d807d6fcd876288f6735cf02
Justiceman81 <Justiceman814@gmail.com>
2024-08-26 18:45:54 +0300

    Update filtersSlice.js

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -21,17 +21,3 @@ export const { changeFilter } = filtersSlice.actions;
 export const selectNameFilter = (state) => state.filters.filters.name;
 
 export const filtersReducer = filtersSlice.reducer;
-
-// import { createSelector } from "@reduxjs/toolkit";
-
-// export const selectContacts = (state) => state.contacts.items;
-// export const selectNameFilter = (state) => state.filters.name;
-
-// export const selectFilteredContacts = createSelector(
-//   [selectContacts, selectNameFilter],
-//   (contacts, filter) => {
-//     return contacts.filter((contact) =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//   }
-// );
